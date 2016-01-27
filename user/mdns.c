b6e9e57815262ff80263917648120f32208610bf
Ben Pirt <ben@pirt.co.uk>
2016-01-27 14:57:48 +0000

    Give mDNS a unique host name to support >1 robots per network

--- user/mdns.c
+++ user/mdns.c
@@ -1,8 +1,11 @@
 #include <esp8266.h>
 #include "mdns.h"
 
+static uint8_t macaddr[6];
+
 void ICACHE_FLASH_ATTR mdnsInit()
 {
+  char buff[16];
   struct ip_info sta_ip;
   wifi_get_ip_info(STATION_IF, &sta_ip);
   
@@ -12,7 +15,9 @@ void ICACHE_FLASH_ATTR mdnsInit()
   
   os_printf("\nStarting mDNS\n");
   struct mdns_info *info = (struct mdns_info *)os_zalloc(sizeof(struct mdns_info));
-  info->host_name = "Mirobot";
+  wifi_get_macaddr(SOFTAP_IF, macaddr);
+  os_sprintf(buff, "mirobot-%02x%02x", macaddr[4], macaddr[5]);
+  info->host_name = buff;
   info->ipAddr = sta_ip.ip.addr; //ESP8266 station IP
   info->server_name = "http";
   info->server_port = 80;
--- user/mdns.h
+++ user/mdns.h
@@ -1,1 +1,6 @@
+#ifndef __mdns_h__
+#define __mdns_h__
+
 void ICACHE_FLASH_ATTR mdnsInit();
+
+#endif
